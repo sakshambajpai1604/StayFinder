830005f5410f5a99a06cd3b53e24cf0ab5b943de
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-06-24 22:52:18 +0530

    Created the listing model

---
+++ backend/models/Listing.js
@@ -0,0 +1,9 @@
+const listingSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  location: String,
+  price: Number,
+  images: [String],
+  hostId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+module.exports = mongoose.model('Listing', listingSchema);
